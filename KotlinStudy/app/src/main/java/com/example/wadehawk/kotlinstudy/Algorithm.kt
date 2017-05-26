942acf84d8e247ef9910ae3dac329cc701302ec9
wade.hawk <wade.hawk@kakaoui-MacBook-Air-8.local>
2017-05-27 01:39:53 +0900

    binary search 구현. // when 사용과 if 사용한 방법. 재귀 함수보다는 내부 순환 로직이 더 효율적.

--- KotlinStudy/app/src/main/java/com/example/wadehawk/kotlinstudy/Algorithm.kt
+++ KotlinStudy/app/src/main/java/com/example/wadehawk/kotlinstudy/Algorithm.kt
@@ -1,25 +1,60 @@
 package com.example.wadehawk.kotlinstudy
 
+import android.util.Log
+
 /**
  * Created by wade.hawk on 2017. 5. 21..
  */
 
 class Algorithm { }
 
-fun MutableList<Int>.swap(index1: Int, index2: Int) {
-    val tmp = this[index1] // 'this' corresponds to the list
-    this[index1] = this[index2]
-    this[index2] = tmp
-}
+/**
+ * Insertion Sort
+ * Big-O : O(n^2)
+ * 2개의 중첩루프가 존재하기 때문
+ * 작은 크기의 배열에서 quick sort O(n log n)등보다 빠르다
+ */
 
 fun Algorithm.insertionSort(array: Array<Int>): Array<Int> {
     var result = array.toMutableList()
     for (x in 1 until array.count()) {
         var y = x
-        while (y > 0 && result[y] < result[y - 1]) {
-            result.swap(y-1,y)
+        val temp = result[y]
+        while (y > 0 && temp < result[y - 1]) {
+            result[y] = result[y - 1]
             y -= 1
         }
+        result[y] = temp
     }
     return result.toTypedArray()
+}
+
+/**
+ * Binary Search
+ * Big-O : O( log n )
+ * 1,000,000 개의 요소에서 서칭할때 log_2(1,000,000) = 19.9 20 단계만 거치면 된다.
+ */
+fun Algorithm.binarySearch(array: Array<Int>, key: Int): Int {
+    var lowerBound = 0
+    var upperBound = array.count()
+    while (lowerBound < upperBound) {
+        val middle = lowerBound + ( upperBound - lowerBound ) / 2
+        // used when
+        when {
+            array[middle] == key -> return middle
+            array[middle] < key -> lowerBound = middle +1
+            else -> upperBound = middle
+        }
+        // used if
+        /*
+        if (array[middle] == key) {
+            return middle
+        }else if (array[middle] < key) { // right
+            lowerBound = middle + 1
+        }else { // left
+            upperBound = middle
+        }
+        */
+    }
+    return -1
 }
--- KotlinStudy/app/src/main/java/com/example/wadehawk/kotlinstudy/MainActivity.kt
+++ KotlinStudy/app/src/main/java/com/example/wadehawk/kotlinstudy/MainActivity.kt
@@ -9,10 +9,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val array = arrayOf(8,2,5,3,1)
+        var array = arrayOf(11, 59, 3, 2, 53, 17, 31, 7, 19, 67, 47, 13, 37, 61, 29, 43, 5, 41, 23)
         val result = Algorithm().insertionSort(array)
+        Log.d("AL","insertion sort result")
         for ( item in result ) {
             Log.d("AL","$item")
         }
+        Log.d("AL","binary search start")
+        val index = Algorithm().binarySearch(result,7)
+        Log.d("AL","binary search result $index")
     }
 }
